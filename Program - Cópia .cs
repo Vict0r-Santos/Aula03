f891f5be50be65b6ff5560f02da7b7d6ae1e1f52
Victor <135838623+Vict0r-Santos@users.noreply.github.com>
2023-06-07 14:45:59 -0300

    Add files via upload

---
+++ Program - Cópia .cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03Variaveis
+{
+    class Program2
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao VestidoBank");
+            Console.Write("O banco mais seguro pelas pessoas que usam roupas.\n\n\n");
+            Console.WriteLine("Serviços disponíveis:");
+            Console.WriteLine("- Acessar o Internet Banking");
+            Console.WriteLine("- Abrir uma conta\n\n");
+            Console.WriteLine("-- Para reclamações disque no SAC: 0800 1234\nOutras regiões: 0800-5555\n\n\n\n");
+            Console.Write("Obrigado por usar nossos serviços.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Como declarar uma variável 
+            // O nome não pode ter:
+            // - Acentos
+            // - Começar com número
+            string chefe = "Mafioso";
+            int idade = 19;
+            Console.WriteLine("Programa criado por " + chefe); 
+            Console.WriteLine("Lembrando que foi criado pelo " + chefe);
+            Console.WriteLine("O dono desse codigo é o " + chefe);
+            Console.WriteLine( chefe + " criou esse programa");
+            Console.WriteLine("Minha idade é " + idade + "y");
+
+            // Juntar os pedaços de texto se chama CONCATENAÇÃO
+            // Esse é o jeito velho
+            Console.WriteLine("Meu nome é " + chefe + " e tenho " + idade + "y");
+
+            // Esse é o Jeito moderno, se chama INTERPOLAÇÃO
+            Console.WriteLine($"Meu nome é {chefe} e eu tenho {idade} anos");
+            Console.ReadKey(); // <---- Comando para finalizar
+        }
+    }
+}
